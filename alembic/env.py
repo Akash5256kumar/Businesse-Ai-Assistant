519c57ab7b9fe88fbc4f49370f42903aee3ba4ef
Akash Kumar <akashkr15082000@gmail.com>
2026-05-13 14:00:09 +0530

    fixed error in env .py file

--- alembic/env.py
+++ alembic/env.py
@@ -90,7 +90,7 @@ from app.models.transaction import Transaction
 from app.models.message_log import MessageLog
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
